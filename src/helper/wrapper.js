5437787738cea18dccd2df05b868d3c9cf8449c0
Ronaldnur <ronaldnursunarto@gmail.com>
2025-08-09 22:37:47 +0700

    update use new prisma version

--- src/helper/wrapper.js
+++ src/helper/wrapper.js
@@ -1,6 +1,6 @@
 const responseWrapper = (res, statusCode, message, data = {}, code = statusCode) => {
     res.status(statusCode).json({
-        status: code,  // Status code masuk dalam response
+        status: code,  
         message,
         data
     });
